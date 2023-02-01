f1577ef30f47cfedc8b3751c0ca85ab01874a7a1
DESKTOP-LHCE2F2\csi22 <manuelclarobarba@gmail.com>
2023-02-01 10:02:52 +0100

    no message

---
+++ OperacionesAritmeticasBasicas/src/main/webapp/script/Operaciones.js
@@ -0,0 +1,25 @@
+
+function ejemplo1() {
+	
+	let suma = 10.25 + 0.75;
+
+	document.write(suma);
+	
+ 	document.write(typeof(suma));
+}
+
+function ejemplo2() {
+	
+	let numero1 = 23;
+	let numero2 = 34;
+	let resultadoOperacion = numero1 + numero2;
+	
+	document.write(resultadoOperacion);
+}
+
+function ejemplo3() {
+	
+	let operacion = 46 + ((8 * 2) / 2) + 46;
+	
+	document.write(operacion); 
+}
